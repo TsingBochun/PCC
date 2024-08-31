264a4544573c3b9148b1ec2a5eb2e4f3d354c80f
TsingBochun <tsingbochun@gmail.com>
2024-08-31 19:51:58 +0800

    use append for 3 times

--- motorcycles.py
+++ motorcycles.py
@@ -1,8 +1,8 @@
-motorcycles = ['honda', 'yamaha', 'suzuki']
-print(motorcycles)
 
-# motorcycles[0] = 'ducati'
-# print(motorcycles)
+motorcycles = []
 
-motorcycles.append("ducati")
+motorcycles.append("honda")
+motorcycles.append("yamaha")
+motorcycles.append("suzuki")
+
 print(motorcycles)
