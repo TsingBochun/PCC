0a9cdbc1e5943c634e3cb1731ec62c547ce10d86
TsingBochun <tsingbochun@gmail.com>
2024-08-31 20:39:17 +0800

    more tricks

--- magicians.py
+++ magicians.py
@@ -3,4 +3,5 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
   #  print(magician)
-  print(magician.title() + ", that was a great trick!")
+  # print(magician.title() + ", that was a great trick!")
+  print("I can't wait to see your next trick, " + magician.title())
