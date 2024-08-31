15f5913239ca5ea226882e9819cc393028127a45
TsingBochun <tsingbochun@gmail.com>
2024-08-31 21:46:03 +0800

    if-else Statements

--- voting.py
+++ voting.py
@@ -1,4 +1,7 @@
-# Simple if Statements
-age = 19
+# if-else Statements
+age = 17 
 if age >= 18:
     print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
