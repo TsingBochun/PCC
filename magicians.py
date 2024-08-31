787c44173f7b693f637d7fde8ad093db607def1f
TsingBochun <tsingbochun@gmail.com>
2024-08-31 20:55:11 +0800

    Using the range() Function

--- magicians.py
+++ magicians.py
@@ -1,9 +1,6 @@
 
-# Looping Through an Entire List
+# Forgetting the Colon
 magicians = ['alice', 'david', 'carolina']
-for magician in magicians:
-  #  print(magician)
-  print(magician.title() + ", that was a great trick!")
-  print("I can't wait to see your next trick, " + magician.title())
-
-print("Thank you, everyone. That was a great magic show!")
+for magician in magicians
+    print(magician)
+  
