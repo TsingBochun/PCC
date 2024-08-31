bf1f119560564272a8018b9aa8aeacc7f06e9788
TsingBochun <tsingbochun@gmail.com>
2024-08-31 21:14:36 +0800

    Looping Through a Slice

--- players.py
+++ players.py
@@ -1,4 +1,7 @@
-# Slicing a List
+# Looping Through a Slice
 
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
-print(players[0 : 3])
+
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
