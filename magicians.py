6638f74794a1a072a6aa7de62283e7b0f98db5a8
TsingBochun <tsingbochun@gmail.com>
2024-08-31 20:37:28 +0800

    Looping Through an Entire List

--- magicians.py
+++ magicians.py
@@ -2,4 +2,5 @@
 # Looping Through an Entire List
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
-    print(magician)
+  #  print(magician)
+  print(magician.title() + ", that was a great trick!")
