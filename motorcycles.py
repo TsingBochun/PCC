c9f76f8f722b89f9706bd97f4dca3a1d2eafd7ea
TsingBochun <tsingbochun@gmail.com>
2024-08-31 20:07:41 +0800

    Removing an Item Using the pop() Method

--- motorcycles.py
+++ motorcycles.py
@@ -1,12 +1,16 @@
 
-# Removing Elements from a List
+# Removing an Item Using the pop() Method
 
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 
 # del motorcycles[0]
-del motorcycles[1]
+# del motorcycles[1]
+# print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
 print(motorcycles)
+print(popped_motorcycle)
 
 
 
