662276d4ee8e3eae513042ca96c29a66e6d040bc
TsingBochun <tsingbochun@gmail.com>
2024-09-01 15:17:00 +0800

    Using a Flag

--- greeter.py
+++ greeter.py
@@ -1,6 +1,2 @@
 
-prompt = "If you tell us who you are, we can personalize the messages you see." 
-prompt += "\nWhat is your first name? "
-
-name = input(prompt)
-print(f"\nHello, {name}!")
+# Using int() to Accept Numerical Input
--- parrot.py
+++ parrot.py
@@ -1,4 +1,13 @@
 
-# Writing Clear Prompts
-name = input("Please enter your name: ")
-print("Hello " + name)
+# Using a Flag
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
