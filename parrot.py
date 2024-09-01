36145a9f6f2afd67f7e9a0c7b64e16d2820ce664
TsingBochun <tsingbochun@gmail.com>
2024-09-01 15:00:38 +0800

    Writing Clear Prompts

--- parrot.py
+++ parrot.py
@@ -1,4 +1,4 @@
 
-# How the input() Function Works
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
+# Writing Clear Prompts
+name = input("Please enter your name: ")
+print("Hello " + name)
